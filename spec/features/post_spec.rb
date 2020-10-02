514f29653312c6be39fd64e2c3c9d6356df3ae45
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-02 01:51:05 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @post = Post.create(title: "My Post". description: "My post desc")
+    @post = Post.create(title: "My Post",= description: "My post desc")
   end
 
   it 'loads the show page' do
